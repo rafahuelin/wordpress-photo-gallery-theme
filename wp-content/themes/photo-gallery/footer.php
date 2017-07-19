bf6c8d3ff9c8c57b34a9f081955023225504966c
Rafa Huelin <it@engisa.com>
2017-07-19 11:41:32 +0100

    Display top admin bar

--- wp-content/themes/photo-gallery/footer.php
+++ wp-content/themes/photo-gallery/footer.php
@@ -1,8 +1,9 @@
             </div>
         </div>
     </div>
-<footer>
-    <p><?php bloginfo('name'); ?> &copy; 2017</p>
-</footer>
+    <footer>
+        <p><?php bloginfo('name'); ?> &copy; 2017</p>
+    </footer>
+    <?php wp_footer(); ?>
 </body>
 </html>
