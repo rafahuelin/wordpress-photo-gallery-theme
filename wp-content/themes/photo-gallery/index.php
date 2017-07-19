b579d2c70e28a0277513e03a2b9c9e94f42275ae
Rafa Huelin <it@engisa.com>
2017-07-19 08:57:17 +0100

    Create content and gallery content files

--- wp-content/themes/photo-gallery/index.php
+++ wp-content/themes/photo-gallery/index.php
@@ -1,38 +1,9 @@
 <?php get_header(); ?>
-            <div class="w3-col m4 l4 pic">
-                <a href="single.html">
-                    <img src="images/1.jpg" class="w3-animate-opacity">
-                    <p>Lorem ipsum dolor sit amet, consectetur adispiscing elit.</p>
-                </a>
-            </div>
-            <div class="w3-col m4 l4 pic">
-                <a href="single.html">
-                    <img src="images/2.jpg" class="w3-animate-opacity">
-                    <p>Lorem ipsum dolor sit amet, consectetur adispiscing elit.</p>
-                </a>
-            </div>
-            <div class="w3-col m4 l4 pic">
-                <a href="single.html">
-                    <img src="images/3.jpg" class="w3-animate-opacity">
-                    <p>Lorem ipsum dolor sit amet, consectetur adispiscing elit.</p>
-                </a>
-            </div>
-            <div class="w3-col m4 l4 pic">
-                <a href="single.html">
-                    <img src="images/4.jpg" class="w3-animate-opacity">
-                    <p>Lorem ipsum dolor sit amet, consectetur adispiscing elit.</p>
-                </a>
-            </div>
-            <div class="w3-col m4 l4 pic">
-                <a href="single.html">
-                    <img src="images/5.jpg" class="w3-animate-opacity">
-                    <p>Lorem ipsum dolor sit amet, consectetur adispiscing elit.</p>
-                </a>
-            </div>
-            <div class="w3-col m4 l4 pic">
-                <a href="single.html">
-                    <img src="images/6.jpg" class="w3-animate-opacity">
-                    <p>Lorem ipsum dolor sit amet, consectetur adispiscing elit.</p>
-                </a>
-            </div>
+    <?php if(have_posts()) : ?>
+        <?php while(have_posts()) : the_post(); ?>
+            <?php get_template_part('content', get_post_format()); ?>
+        <?php endwhile; ?>
+    <?php else : ?>
+        <?php echo wpautop('Sorry, there are no posts'); ?>
+    <?php endif; ?>
 <?php get_footer(); ?>
