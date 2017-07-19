aa2711c62a36eade8b61ab3b898c559486493e67
Rafa Huelin <it@engisa.com>
2017-07-19 11:40:29 +0100

    Customize categories sidebar

--- wp-content/themes/photo-gallery/functions.php
+++ wp-content/themes/photo-gallery/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('widgets/class-wp-widget-categories.php');
+
 // Theme Support
 function theme_setup() {
     // Featured Image Support
@@ -12,5 +14,23 @@ function theme_setup() {
 }
 
 add_action('after_setup_theme', 'theme_setup');
+
+// Widget Locations
+function init_widgets($id) {
+    register_sidebar(array(
+        'name' => 'Sidebar',
+        'id' => 'sidebar'
+    ));
+}
+
+add_action('widgets_init', 'init_widgets');
+
+// Register Widgets
+
+function custom_register_widgets() {
+    register_widget('WP_Widget_Categories_Custom');
+}
+
+add_action('widgets_init', 'custom_register_widgets');
 
 ?>
--- wp-content/themes/photo-gallery/header.php
+++ wp-content/themes/photo-gallery/header.php
@@ -21,13 +21,9 @@
 
 <div class="w3-row">
     <div class="w3-col m3 l3">
-        <ul class="w3-ul">
-            <li><a href="#">Nature</a></li>
-            <li><a href="#">Animals</a></li>
-            <li><a href="#">Objects</a></li>
-            <li><a href="#">People</a></li>
-            <li><a href="#">Abstract</a></li>
-        </ul>
+        <?php if(is_active_sidebar('sidebar')) : ?>
+            <?php dynamic_sidebar('sidebar'); ?>
+        <?php endif; ?>
     </div>
     <div class="w3-col m9 l9">
         <div class="w3-row">
